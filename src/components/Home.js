8c8faf57a9a4ce58a32053a5bbdfce41a4d097b7
Carson Meiling <carmc93@gmail.com>
2020-05-19 15:31:40 -0600

    search is working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,45 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import SeachForm from './SearchForm';
+import SearchForm from './SearchForm';
 import BusinessList from './BusinessList';
 
 const Home = () => {
   const [bus, setBus] = useState([])
 
-  useEffect (() => {
-      axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
-        },
-        params: {
-          term: 'food',
-          location: 'SLC',
-          limit: 5,
-        }
-      })
-        .then( res => {setBus(res.data.businesses)
-          console.log(bus)
+  // useEffect (() => {
+  //     axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search`,
+  //     {
+  //       headers: {
+  //         Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+  //       },
+  //       params: {
+  //         term: 'food',
+  //         location: 'SLC',
+  //         limit: 5,
+  //       }
+  //     })
+  //       .then( res => {setBus(res.data.businesses)
+  //         console.log(bus)
         
-        })
-        .catch( err => {console.log(err)
-        })
-  },[])
+  //       })
+  //       .catch( err => {console.log(err)
+  //       })
+  // },[])
 
   let itemsToRender = bus;
   console.log(itemsToRender)
   if (bus){
     itemsToRender = bus.map(b => {
-      return <div key={b.id}>{b.name}</div>
+      return <div key={b.id}>
+        <h2>{b.name}</h2>
+        <p>{b.rating}</p>
+        <p>Price{b.price}</p>
+        </div>
     })
   }
   return(
     <div>
+      <SearchForm setBus={setBus} />
       <h2>Home/landing page</h2>
       <div>{itemsToRender}</div>
     </div>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,15 +1,35 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 
-const SearchForm = () => {
-   const [find , setFind] = useState("");
-   const [near , setNear] = useState("");
+const SearchForm = (props) => {
+
+  const [term , setTerm] = useState("")
+  const [location , setLocation] = useState("")
 
    const handleSubmit = (e) => {
       e.preventDefault();
-      
-
+      search(term, location)
    }
+
+   const search = (term, location) => {
+      axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+        },
+        params: {
+          term,
+          location,
+          limit: 5,
+        }
+      })
+        .then( res => {props.setBus(res.data.businesses)
+        
+        })
+        .catch( err => {console.log(err)
+        })
+  }
 
    return(
       <form onSubmit = {handleSubmit}>
@@ -17,18 +37,18 @@ const SearchForm = () => {
             <label>Find</label>
             <input 
             type="text" 
-            name="find"
-            value={find}
-            onChange={e=>setFind(e.target.value)}
+            name="term"
+            value={term}
+            onChange={e=>setTerm(e.target.value)}
             />
          </div>
          <div>
             <label>Near</label>
             <input 
             type="text" 
-            name="near"
-            value={near}
-            onChange={e=>setNear(e.target.value)}
+            name="location"
+            value={location}
+            onChange={e=>setLocation(e.target.value)}
             />
          </div>
          <div>

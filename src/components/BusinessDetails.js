e29438cec67f054905a7d1e5a47018d840e5f125
Carson Meiling <carmc93@gmail.com>
2020-05-26 17:01:40 -0600

    working on go back

--- src/components/BusinessDetails.js
+++ src/components/BusinessDetails.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import {BizContext} from '../providers/BizProvider';
-import axios from 'axios';
 
 
 
@@ -9,22 +8,16 @@ const BusinessDetails = (props) => {
    const [biz, setBiz] = useContext(BizContext);
    const [business, setBusiness] = useState({});
 
-  
-
-
-   // const getBiz = () => {
-   //    const currentBiz = biz.filter(b => b.id === props.match.params.id)
-   //    setBusiness(currentBiz[0])
-   //    debugger
-   //    console.log(business)
-   // }
+   console.log(biz)
 
    return(
       <>
-
+         <Link to={{
+            pathname:'/',
+            state:{biz}
+         }}>Home</Link>
          <button onClick = {() => props.history.goBack()}>Go Back</button>
          <h2>{props.location.state.business.name}</h2>
-      
       </>
    )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,22 +7,22 @@ import {BizContext} from '../providers/BizProvider';
 
 
 const Home = () => {
-  // const [bus, setBus] = useState([])
   const [biz, setBiz] = useContext(BizContext);
 
   console.log(biz)
   console.log(typeof biz)
 
+  
   return(
     <div className="home-page">
-      <h2 className="header">Meal Finder</h2>
-      {/* <SearchForm setBus={setBus} /> */}
+      <h2 className="header">Business Search</h2>
       <SearchForm />
       {
         biz.map(business => (
           <BusinessList business={business} key={business.id}/>
           ))
-      }
+      
+        }
     </div>
 
 

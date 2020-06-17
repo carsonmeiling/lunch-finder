5a7cdb2719a7a42d1a430dc8e75708017eeeba32
Carson Meiling <carmc93@gmail.com>
2020-06-17 12:10:54 -0600

    added google maps to biz details

--- src/components/BusinessDetails.js
+++ src/components/BusinessDetails.js
@@ -1,19 +1,36 @@
 import React from 'react';
+import {
+   withScriptjs,
+   withGoogleMap,
+   GoogleMap,
+   Marker} from 'react-google-maps';
+
+   
 
 
 
 const BusinessDetails = (props) => {
-      const biz = props.location.state.business
+   const biz = props.location.state.business
 
-      const background = {
-         backgroundImage: `url(${biz.image_url})`
-      }
+   const background = {
+      backgroundImage: `url(${biz.image_url})`
+   }
+
+   const MyMapComponent = withScriptjs(
+      withGoogleMap(props => (
+        <GoogleMap defaultZoom={14} defaultCenter={{ lat: biz.coordinates.latitude, lng: biz.coordinates.longitude }}>
+          {props.isMarkerShown && (
+            <Marker position={{ lat: biz.coordinates.latitude, lng: biz.coordinates.longitude }} />
+          )}
+        </GoogleMap>
+      ))
+    );
+
+
    return(
       <div className="bizPage">
          <button className="back" onClick = {() => props.history.goBack()}>Back to Results</button>
-         {/* <div style={background}>
-
-         </div> */}
+         
          <div className="businessDetails">
             <h2 className='bizName' >{biz.name}</h2>
 
@@ -23,6 +40,13 @@ const BusinessDetails = (props) => {
             <p className="bizDetails">Rating:{biz.rating}</p>
             
          </div>
+         <MyMapComponent
+                  isMarkerShown
+                  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAN7mwyR_B3NDDLnuwkLds1RU9IUSBklc8&v=3.exp&libraries=geometry,drawing,places"
+                  loadingElement={<div style={{ height: `100%` }} />}
+                  containerElement={<div style={{ height: `400px` }} />}
+                  mapElement={<div style={{ height: `100%` }} />}
+                />
 
       </div>
    )
